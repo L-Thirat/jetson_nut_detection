1ddfa3fc3793b1f4c0ff56ca9448e25caa925e48
L-Thirat <terus_limsurut@hotmail.com>
2021-02-24 16:36:35 +0900

    .

--- tfrecord/generate_tfrecord.py
+++ tfrecord/generate_tfrecord.py
@@ -34,7 +34,7 @@ def class_text_to_int(row_label):
         return 2
     elif row_label == 'cross':
         return 3
-    elif row_label == 'base':
+    elif row_label == 'ng':
         return 4
     else:
         None
